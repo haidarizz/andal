cb2ba2c07962f5d916bc8d872455cacbd7f23c43
Muhammad Haidar Izzuddin <muhammadhaidar@mail.ugm.ac.id>
2022-12-24 19:14:03 +0700

    feat: add squad photos

--- src/components/Cabinet.js
+++ src/components/Cabinet.js
@@ -1,7 +1,26 @@
 import "./PopularStyles.css";
 import CardData from "./CardData";
-import man from "../assets/man.jpg";
-import woman from "../assets/woman.jpg";
+import haidar from "../assets/haidar.jpg";
+import diqi from "../assets/diqi.jpeg";
+import agnes from "../assets/agnes.jpg";
+import nisa from "../assets/nisa.jpg";
+import abil from "../assets/abil.jpg";
+import aji from "../assets/aji.jpg";
+import alfian from "../assets/alfian.png";
+import banen from "../assets/banen.jpg";
+import besty from "../assets/besty.jpg";
+import farhan from "../assets/farhan.jpg";
+import inam from "../assets/inam.jpg";
+import intan from "../assets/intan.jpg";
+import iqbal from "../assets/iqbal.png";
+import juang from "../assets/juang.png";
+import roy from "../assets/roy.jpg";
+import salma from "../assets/salma.jpeg";
+import shabira from "../assets/shabira.jpg";
+import khalid from "../assets/khalid.jpg";
+import leon from "../assets/leon.jpg";
+import uswa from "../assets/uswa.jpg";
+import yazid from "../assets/yazid.png";
 
 function Cabinet (){
     return(
@@ -10,28 +29,28 @@ function Cabinet (){
             <h1>Harian</h1>
             <div className="popularcard-split-4">
                 <CardData
-                    image={man}
+                    image={haidar}
                     heading="Muhammad Haidar Izzuddin"
                     subtitle="Menteri Analisis Data Digital"
                     text="Teknologi Informasi 2020"
                     url="https://www.instagram.com/haidarizz/"
                 />
                 <CardData
-                    image={man}
+                    image={diqi}
                     heading="Habib Luthfi Ash-Shiddiqie"
                     subtitle="Wakil Menteri Analisis Data Digital"
                     text="Teknik Mesin 2020"
                     url="https://www.instagram.com/ashshiddiqie_01/"
                 />
                 <CardData
-                    image={woman}
+                    image={agnes}
                     heading="Agnes Suarna"
                     subtitle="Sekretaris"
                     text="Teknologi Informasi 2020"
                     url="https://www.instagram.com/agnessuarna_/"
                 />
                 <CardData
-                    image={woman}
+                    image={nisa}
                     heading="Khairun Nisa' Zuqri"
                     subtitle="Bendahara"
                     text="Teknologi Informasi 2021"
@@ -42,35 +61,35 @@ function Cabinet (){
             <h1>Internal</h1>
             <div className="popularcard-split-5">
                 <CardData
-                    image={man}
+                    image={banen}
                     heading="Raden Muhammad Banendra Bagaskara"
                     subtitle="Dirjen Internal"
                     text="Teknologi Informasi 2020"
                     url="https://www.instagram.com/muhammadbanendra/"
                 />
                 <CardData
-                    image={woman}
+                    image={intan}
                     heading="Rizky Intan Nurlita"
                     subtitle="Staf Internal"
                     text="Teknologi Informasi 2020"
                     url="https://www.instagram.com/intanurlitaa/"
                 />
                 <CardData
-                    image={woman}
+                    image={besty}
                     heading="Faizah Bestiyana Darmawati"
                     subtitle="Staf Internal"
                     text="Teknologi Informasi 2020"
                     url="https://www.instagram.com/faizahbestyanaa/"
                 />
                 <CardData
-                    image={man}
+                    image={inam}
                     heading="In'am Nurul Fuady"
                     subtitle="Staf Internal"
                     text="Teknologi Informasi 2021"
                     url="https://www.instagram.com/inamnurul/"
                 />
                 <CardData
-                    image={woman}
+                    image={shabira}
                     heading="Shabira Salwa Hanan"
                     subtitle="Staf Internal"
                     text="Teknik Biomedis 2021"
@@ -79,83 +98,90 @@ function Cabinet (){
             </div>
             <h2>Kedirjenan</h2>
             <h1>Media dan Penulisan</h1>
-            <div className="popularcard-split-5">
+            <div className="popularcard-split-6">
             <CardData
-                    image={woman}
+                    image={salma}
                     heading="Salma Fathina Azzahwa"
                     subtitle="Dirjen Media dan Penulisan"
                     text="Teknik Biomedis 2020"
                     url="https://www.instagram.com/salmaazzahwa/"
                 />
                 <CardData
-                    image={man}
+                    image={iqbal}
                     heading="Muhammad Iqbal Masykuri"
                     subtitle="Staf Media dan Penulisan"
                     text="Teknologi Informasi 2020"
                     url="https://www.instagram.com/kaniqbal_/"
                 />
                 <CardData
-                    image={man}
+                    image={roy}
                     heading="Roy Rinatza Zain"
                     subtitle="Staf Media dan Penulisan"
                     text="Teknik Sipil 2021"
                     url="https://www.instagram.com/royrzain/"
                 />
                 <CardData
-                    image={man}
+                    image={leon}
                     heading="Leonard Mars Kurniaputra"
                     subtitle="Staf Media dan Penulisan"
                     text="Teknologi Informasi 2021"
                     url="https://www.instagram.com/leleonnn/"
                 />
                 <CardData
-                    image={man}
+                    image={abil}
                     heading="Abdjad Aiman Sabilla"
                     subtitle="Staf Media dan Penulisan"
                     text="Teknik Mesin 2021"
                     url="https://www.instagram.com/abdjadabil/"
+                />
+                <CardData
+                    image={alfian}
+                    heading="Muhammad Alfian Mukti Sampurno"
+                    subtitle="Staf Media dan Penulisan"
+                    text="Teknik Elektro 2020"
+                    url="https://www.instagram.com/muhammadalfianmukti/"
                 />
             </div>
             <h2>Kedirjenan</h2>
             <h1>Teknikal</h1>
             <div className="popularcard-split-6">
             <CardData
-                    image={man}
+                    image={juang}
                     heading="Karunia Perjuangan Mustadl'Afin"
                     subtitle="Dirjen Teknikal"
                     text="Teknologi Informasi 2020"
                     url="https://www.instagram.com/karuniaperjuangan/"
                 />
                 <CardData
-                    image={man}
+                    image={yazid}
                     heading="Ahmad Yazid Naufan"
                     subtitle="Staf Teknikal"
                     text="Teknologi Informasi 2020"
                     url="https://www.instagram.com/yazidnaufan/"
                 />
                 <CardData
-                    image={man}
+                    image={khalid}
                     heading="Khalid Rizki Ananta"
                     subtitle="Staf Teknikal"
                     text="Teknologi Informasi 2020"
                     url="https://www.instagram.com/khalid.rizki22/"
                 />
                 <CardData
-                    image={man}
+                    image={aji}
                     heading="Qornain Aji"
                     subtitle="Staf Teknikal"
                     text="Teknologi Informasi 2021"
                     url="https://www.instagram.com/qornainaji/"
                 />
                 <CardData
-                    image={woman}
+                    image={uswa}
                     heading="Annisa Uswa Sufia"
                     subtitle="Staf Teknikal"
                     text="Teknologi Informasi 2021"
                     url="https://www.instagram.com/uswaow/"
                 />
                 <CardData
-                    image={man}
+                    image={farhan}
                     heading="Farhan Franaka"
                     subtitle="Staf Teknikal"
                     text="Teknologi Informasi 2021"
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ class Navbar extends Component {
     render() {
         return(
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">Andal 2022</h1>
+                <h1 className="navbar-logo">Andal</h1>
                 <div className="menu-icons" onClick={this.handleClick}>
                     <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
                 </div>
